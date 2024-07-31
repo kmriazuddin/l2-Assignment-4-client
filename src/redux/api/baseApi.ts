ab5a2948bc4eda7b39e37951156f8543ddf96557
kmriazuddin <mohammadriazuddin3@gmail.com>
2024-08-01 00:01:08 +0600

    mechanical keyboard shop client deploy in vercel

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const baseApi = createApi({
   reducerPath: "baseApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:8000/api/v1",
+    baseUrl: "https://mks-server.vercel.app/api/v1",
   }),
 
   tagTypes: ["products"],
